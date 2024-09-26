cce304a9f9c04cf310490939fab365a9fe0260ac
adas20 <dasar-24@rhodes.edu>
2024-09-25 21:33:32 -0400

    Initial Commit

---
+++ network_scanner.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h> // read(), write(), close() 
+#include <arpa/inet.h> 
+#include <dirent.h> // Required for directory operations for files
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+
+
+int network_scanner() {
+    printf("TODO\n");
+}
+
+
+int main() {
+    network_scanner();
+}
+
+
